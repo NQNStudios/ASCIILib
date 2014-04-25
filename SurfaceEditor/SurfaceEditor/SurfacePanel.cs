52418bc28a42c167c0452d47607ab252da483ee1
Nathaniel Nelson <nelson.nleroy@gmail.com>
2014-04-24 21:46:31 -0600

    Fixed the SurfacePanel to allow proper use of Graphics

--- SurfaceEditor/SurfaceEditor/SurfacePanel.cs
+++ SurfaceEditor/SurfaceEditor/SurfacePanel.cs
@@ -15,9 +15,41 @@ namespace SurfaceEditor
     /// </summary>
     public partial class SurfacePanel : UserControl
     {
+        private Graphics graphics;
+
         public SurfacePanel()
         {
             InitializeComponent();
         }
+
+        private void SurfacePanel_Load(object sender, EventArgs e)
+        {
+            graphics = this.CreateGraphics();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Clear();
+        }
+
+        #region Graphics Helper Methods
+
+        private void Clear()
+        {
+            graphics.Clear(Color.Gray);
+        }
+
+        private void FillRectangle(Rectangle rectangle, Color color)
+        {
+            Brush brush = new SolidBrush(color);
+
+            graphics.FillRectangle(brush, rectangle);
+
+            brush.Dispose();
+        }
+
+        #endregion
     }
 }
