53fdd15e499dbf2b9988da7d9420f9f609f4cef1
Nathaniel Nelson <nelson.nleroy@gmail.com>
2014-04-24 20:12:16 -0600

    Created the project for the ASCIILib SurfaceEditor which will be made in Visual C#. Laid a bit of groundwork for that project.

---
+++ SurfaceEditor/SurfaceEditor/EditorForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace SurfaceEditor
+{
+    partial class EditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.surfacePanel1 = new SurfaceEditor.SurfacePanel();
+            this.SuspendLayout();
+            // 
+            // surfacePanel1
+            // 
+            this.surfacePanel1.Location = new System.Drawing.Point(13, 13);
+            this.surfacePanel1.Name = "surfacePanel1";
+            this.surfacePanel1.Size = new System.Drawing.Size(716, 480);
+            this.surfacePanel1.TabIndex = 0;
+            // 
+            // EditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 511);
+            this.Controls.Add(this.surfacePanel1);
+            this.Name = "EditorForm";
+            this.Text = "ASCIILib Surface Editor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SurfacePanel surfacePanel1;
+    }
+}
+
---
+++ SurfaceEditor/SurfaceEditor/EditorForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SurfaceEditor
+{
+    /// <summary>
+    /// The main form for the Surface Editor.
+    /// </summary>
+    public partial class EditorForm : Form
+    {
+        public EditorForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
