35b61f9b7d1d9823b97848f113df20a3131b45f2
Nathaniel Nelson <nelson.nleroy@gmail.com>
2016-07-02 12:02:21 -0600

    Untested refactor of FileReader and addition of forbidden characters feature

--- ASCIILib/FileReader.cpp
+++ ASCIILib/FileReader.cpp
@@ -14,18 +14,49 @@ namespace
 
 ascii::FileReader::FileReader(string path)
 {
+    Initialize(path);
+}
+
+ascii::FileReader::FileReader(string path, string forbiddenCharactersPath)
+{
+    // Read the list of forbidden characters from the given forbidden
+    // characters file, if there is one
+    mForbiddenCharacters = ReadContents(forbiddenCharactersPath);
+    Log::Print("Loading file ", false);
+    Log::Print(path, false);
+    Log::Print(" with forbidden characters: ", false);
+    Log::Print(mForbiddenCharacters);
+
+    Initialize(path);
+}
+
+void FileReader::Initialize(string path)
+{
+    // Now read the UTF-8 file we're really concerned with
+    UnicodeString contents = ReadContents(path);
+
+    // If the file exists, parse each line from the file
+    if (mExists)
+    {
+        ParseLines(contents, path);
+    }
+}
+
+UnicodeString ascii::FileReader::ReadContents(string path)
+{
     // Open the file
     //Log("Opening file: " + path);
     UFILE* ufile = u_fopen(path.c_str(), "r", NULL, "UTF-8");
 
+    // Check if the file exists and was opened properly
     mExists = (ufile != NULL);
 
     // Output a warning if the file wasn't found
-    if (!Exists())
+    if (!mExists)
     {
         Log::Print("Possible error: Tried to open nonexistent file: " + path);
+        return UnicodeString("");
     }
-    // Otherwise, extract each line from the file
     else
     {
         // Retrieve the size of the file
@@ -48,10 +79,15 @@ ascii::FileReader::FileReader(string path)
         // Add the string termination
         contents[index] = 0;
 
+        // Create a UnicodeString object
+        UnicodeString contentsUString(contents);
+
+        // Clean up the memory used to read the UChars
+        delete[] contents;
         // Close the file
         u_fclose(ufile);
 
-        // Check if the UTF-8 BOM is present
+        // Check if the UTF-8 Bit Order Mark is present
         UChar a,b,c;
         a = contents[0];
         b = contents[1];
@@ -59,15 +95,36 @@ ascii::FileReader::FileReader(string path)
 
         if(!(a!=(UChar)0xEF || b!=(UChar)0xBB || c!=(UChar)0xBF))
         {
-            Log::Print("Warning. File contains UTF-8 bit order mark:");
+            Log::Print("Warning. File contains UTF-8 bit order mark:", false);
             Log::Print(path);
+
+            // Strip the BOM
+            contentsUString = contentsUString.tempSubString(3);
         }
 
+        return contentsUString;
+    }
+}
+
+void ascii::FileReader::ParseLines(UnicodeString contents, string path)
+{
+    int numChars = contents.length();
+    if (numChars > 0)
+    {
         // Split the file contents by line endings
         UnicodeString line = "";
-        for (int i = 0; i < index; ++i)
+        for (int i = 0; i < numChars; ++i)
         {
             UChar nextChar = contents[i];
+
+            // Make sure none of the characters we read are forbidden
+            if (mForbiddenCharacters.indexOf(nextChar) != -1)
+            {
+                Log::Print("Warning! Forbidden character ", false);
+                Log::Print(nextChar, false);
+                Log::Print(" found in file ", false);
+                Log::Print(path);
+            }
 
             // Store the characters that are chained together as a line
             if (nextChar == '\n' || nextChar == 0)
@@ -85,11 +142,10 @@ ascii::FileReader::FileReader(string path)
             }
         }
 
-        // TODO is it right to do this?
-        // Clean up the memory used to read the UChars
-        delete[] contents;
     }
 }
+
+
 
 bool ascii::FileReader::HasNextLine()
 {
--- ASCIILib/FileReader.h
+++ ASCIILib/FileReader.h
@@ -15,12 +15,16 @@ namespace ascii
 
     // Reads a Unicode file line by line or all at once. Will print error messages
     // if a nonexistent file is opened, and will simplify file input code by
-    // automatically closing the file read. Returns UTF-8 strings.
+    // automatically closing the file read. Returns UTF-8 encoded std::strings and
+    // ICU UnicodeStrings
     class FileReader
     {
         public:
             // Construct a FileReader of the given filepath.
             FileReader(string path);
+            // Construct a FileReader of the given filepath checking for
+            // forbidden characters defined in the second given file
+            FileReader(string path, string forbiddenCharactersPath);
 
             // Check if the file was successfully opened
             inline bool Exists() { return mExists; }
@@ -33,13 +37,19 @@ namespace ascii
             // a UnicodeString 
             UnicodeString NextLineUnicode();
 
-
             // Return a string containing all text in the file being read.
             string FullContents();
 
         private:
+            void Initialize(string path);
+            // Retrieve the full UTF-8 contents
+            UnicodeString ReadContents(string path);
+            // Parse the UTF-8 contents of a file into individual lines of text
+            void ParseLines(UnicodeString contents, string path);
+
             deque<UnicodeString> mLines;
             bool mExists;
+            UnicodeString mForbiddenCharacters;
     };
 
 }
